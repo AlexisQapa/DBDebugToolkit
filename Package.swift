a83a6f08fd466ad49df63e5c96ef3bb8d421f311
Alexis Schultz <alexis.schultz@qapa.com>
2021-04-01 14:51:51 +0200

    SPM

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "Applicant",
+	defaultLocalization: "fr",
+	platforms: [.iOS("13.4")],
+	products: [.library(name: "DBDebugToolkit", targets: ["DBDebugToolkit"])],
+	dependencies: [],
+	targets: [.target(name: "DBDebugToolkit")],
+)
